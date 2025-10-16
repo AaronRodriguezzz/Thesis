7cbfca1a9a7f76f28184f9d2ef54939e52e1d5c6
iamaaron <francisaaronrodriguez23@gmail.com>
2025-10-17 00:43:32 +0800

    1

--- backend/middleware/Auth.js
+++ backend/middleware/Auth.js
@@ -21,25 +21,24 @@ const verifyToken = (req, res, next) => {
 };
 
 const verifyAdminToken = (req, res, next) => {
-    // const token = req.cookies.user; // same cookie
+    const token = req.cookies.user; // same cookie
 
-    // if (!token) {
-    //     return res.status(401).json({ message: "No token found" });
-    // }
+    if (!token) {
+        return res.status(401).json({ message: "No token found" });
+    }
 
-    // try {
-    //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    //     // Only allow users with an admin role
-    //     if (!decoded.role === 'Front Desk' || !decoded.role === 'Admin') {
-    //         return res.status(403).json({ message: "Access denied" });
-    //     }
+        // Only allow users with an admin role
+        if (!decoded.role === 'Front Desk' || !decoded.role === 'Admin') {
+            return res.status(403).json({ message: "Access denied" });
+        }
 
-    //     next();
-    // } catch (err) {
-    //     return res.status(403).json({ message: "Invalid or expired token" });
-    // }
-    next();
+        next();
+    } catch (err) {
+        return res.status(403).json({ message: "Invalid or expired token" });
+    }
 };
 
 
