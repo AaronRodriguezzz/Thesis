97dd0650d09d1ec107bd1308b2f94ffc4b432030
iamaaron <francisaaronrodriguez23@gmail.com>
2025-10-25 23:33:23 +0800

    1

--- frontend/src/Admin/Appointments.jsx
+++ frontend/src/Admin/Appointments.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import Pagination from "@mui/material/Pagination";
 import { useFetch } from "../../hooks/useFetch";
