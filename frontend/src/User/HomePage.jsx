879d70454e7663f288ad87bc6f86c6acdab1e9be
iamaaron <francisaaronrodriguez23@gmail.com>
2025-11-19 23:08:37 +0800

    ui update

--- frontend/src/User/HomePage.jsx
+++ frontend/src/User/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useSectionViews } from "../../hooks/HomeRef";
 import { motion } from "motion/react"
 import HeroSection from "./HomeSections/HeroSection";
+import AboutSection from "./HomeSections/AboutSection";
 import ServiceSection from "./HomeSections/ServiceSection";
 import ProductSection from "./HomeSections/ProductSection";
 import FeedbackSection from "./HomeSections/FeedbackSection";
@@ -9,14 +10,13 @@ import ReviewFormSection from "./HomeSections/ReviewFormSection";
 
 export default function MainPage() {
   const container = "w-full max-w-7xl mx-auto px-4 md:px-6";  
-  const { sectionRefs } = useSectionViews();
   
   return (
     <div className="w-full overflow-x-hidden">
       <HeroSection />
       {/* About Us */}
-      
 
+      <AboutSection />
       <div className={container}>
         <ProductSection/>
       </div>
